bfaa713fe2983f3225cbef340458e837d9adf5b3
Ben Schlegel <brschlegels@gmail.com>
2022-01-15 17:50:18 -0500

    Starter project

---
+++ ProceduralEngine/main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    //Test for Box2D
+    b2Vec2 gravity(0.0f, -10.0f);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
