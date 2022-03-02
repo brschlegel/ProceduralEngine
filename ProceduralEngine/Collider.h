01dbfb453f19cc45d9623b0ed5a2811e8b46dacc
Ben Schlegel <brschlegels@gmail.com>
2022-03-01 21:58:28 -0500

    commented out the broken stuff

--- ProceduralEngine/Collider.h
+++ ProceduralEngine/Collider.h
@@ -1,15 +1,15 @@
-#pragma once
-#include <Box2D/Box2D.h>
-#include "Component.h"
-#include "GameObject.h"
-class Collider :
-    public Component
-{
-public:
-    bool CheckCollision(Collider* other);
-    b2AABB getAABB();
-    b2Shape* getShape();
-protected:
-    b2Shape* shape;
-};
+//#pragma once
+//#include <Box2D/Box2D.h>
+//#include "Component.h"
+//#include "GameObject.h"
+//class Collider :
+//    public Component
+//{
+//public:
+//    bool CheckCollision(Collider* other);
+//    b2AABB getAABB();
+//    b2Shape* getShape();
+//protected:
+//    b2Shape* shape;
+//};
 
