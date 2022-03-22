fa28f120351ce686fb564c027239b3c6b5b4251f
Ben Schlegel <brschlegels@gmail.com>
2022-03-22 18:42:29 -0400

    Its Alive!

--- ProceduralEngine/BoxCollider.cpp
+++ ProceduralEngine/BoxCollider.cpp
@@ -1,23 +1,23 @@
-//#include "BoxCollider.h"
-//
-//BoxCollider::BoxCollider(b2Vec2 size)
-//{
-//	b2PolygonShape* s = new b2PolygonShape();
-//	this->size = size;
-//	s->SetAsBox(size.x / 2.0f, size.y / 2.0f);
-//	shape = s;
-//}
-//
-//void BoxCollider::init()
-//{
-//}
-//
-//void BoxCollider::DebugDraw()
-//{
-//	Debug::DrawRectangle(gameObject->getTransform()->getPosition(), size.x, size.y);
-//}
-//
-//std::string BoxCollider::toString()
-//{
-//	return "Box Collider: " + to_string(size.x) + " " + to_string(size.y);
-//}
+#include "BoxCollider.h"
+
+BoxCollider::BoxCollider(b2Vec2 size)
+{
+	b2PolygonShape* s = new b2PolygonShape();
+	this->size = size;
+	s->SetAsBox(size.x / 2.0f, size.y / 2.0f);
+	shape = s;
+}
+
+void BoxCollider::init()
+{
+}
+
+void BoxCollider::DebugDraw()
+{
+	Debug::DrawRectangle(gameObject->getTransform()->getPosition(), size.x, size.y);
+}
+
+std::string BoxCollider::toString()
+{
+	return "Box Collider: " + to_string(size.x) + " " + to_string(size.y);
+}
--- ProceduralEngine/BoxCollider.h
+++ ProceduralEngine/BoxCollider.h
@@ -1,15 +1,15 @@
-//#pragma once
-//#include "Collider.h"
-//
-//class BoxCollider :
-//    public Collider
-//{
-//public:
-//    BoxCollider(b2Vec2 size);
-//    void init() override;
-//    void DebugDraw();
-//    std::string toString() override;
-//private:
-//    b2Vec2 size;
-//};
+#pragma once
+#include "Collider.h"
+
+class BoxCollider :
+    public Collider
+{
+public:
+    BoxCollider(b2Vec2 size);
+    void init() override;
+    void DebugDraw();
+    std::string toString() override;
+private:
+    b2Vec2 size;
+};
 
